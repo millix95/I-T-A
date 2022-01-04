3cd9b5bdfb43126e341253ae4e78879df976d2e1
Milos Dobrijevic <dobry95@gmail.com>
2022-01-04 07:29:30 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var pi = 3.14;
+var r = prompt("Unesite poluprečnik osnove valjka u cm:");
+var h = prompt("Unesite željenu visinu valjka u cm:");
+var r2 = r * r;
+var V = r2 * pi * h;
+var x = prompt("Zapremina valjka iznosi: " + V + "c㎥");
