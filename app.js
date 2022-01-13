fcd7c104cbe2e300e42d938f18f54ee10c4060d2
Milos Dobrijevic <dobry95@gmail.com>
2022-01-13 16:16:44 +0100

    valjak

---
+++ app.js
@@ -0,0 +1,7 @@
+var pocetak = alert ('Program za izračunavanje zapremine valjka!');
+var h = prompt ('Unesite vrijednost visine valjka:');
+var r = prompt ('Unesite poluprečnik valjka:')
+var r2 = (r * r);
+var pi = 3.14;
+var v = (r2 * h * pi);
+var v2 = alert ('Zapremina valjka iznosi: ' + v);
