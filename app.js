c163ae401838f823900f84f2a9d420c6151867a6
Milos Dobrijevic <dobry95@gmail.com>
2022-01-17 14:10:55 +0100

    Pupin

--- app.js
+++
@@ -1,7 +0,0 @@
-var pocetak = alert ('Program za izračunavanje zapremine valjka!');
-var h = prompt ('Unesite vrijednost visine valjka:');
-var r = prompt ('Unesite poluprečnik valjka:')
-var r2 = (r * r);
-var pi = 3.14;
-var v = (r2 * h * pi);
-var v2 = alert ('Zapremina valjka iznosi: ' + v);
